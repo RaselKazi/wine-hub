d10034e1116b7e243ebc99c58f3ce134a9e93942
Rasel Kazi <67517631+RaselKazi@users.noreply.github.com>
2025-05-09 19:55:47 +0600

    finsh blog

--- src/components/page/home/section/Blog.tsx
+++ src/components/page/home/section/Blog.tsx
@@ -1,10 +1,43 @@
 import HadingTitle from "@/components/ui/hading-title";
 import React from "react";
+import BlogCard from "./BlogCard";
 
+const BlogItem = [
+  {
+    title: "the fresh new wine Stories",
+    date: "june 20, 2023",
+    image: "/images/blog/blog1.jpg",
+    alt: "",
+  },
+  {
+    title: "the fresh new wine Stories",
+    date: "june 20, 2023",
+    image: "/images/blog/blog2.jpg",
+    alt: "",
+  },
+  {
+    title: "the fresh new wine Stories",
+    date: "june 20, 2023",
+    image: "/images/blog/blog3.jpg",
+    alt: "",
+  },
+  {
+    title: "the fresh new wine Stories",
+    date: "june 20, 2023",
+    image: "/images/blog/blog4.jpg",
+    alt: "",
+  },
+];
 const Blog = () => {
   return (
     <div>
       <HadingTitle hadingTitle="este 1940" title="the fresh new wine Stories" />
+
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 py-8">
+        {BlogItem.map((item, index) => (
+          <BlogCard key={index} {...item} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/page/home/section/BlogCard.tsx
+++ src/components/page/home/section/BlogCard.tsx
@@ -1,24 +1,33 @@
+import Image from "next/image";
 import React from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-interface BlogCardProps {}
 
-const BlogCard: React.FC<BlogCardProps> = ({}) => {
+interface BlogCardProps {
+  title: string;
+  date: string;
+  image: string;
+  alt: string;
+}
+
+const BlogCard: React.FC<BlogCardProps> = ({ title, date, image, alt }) => {
   return (
-    <Card className="w-[350px]">
-      <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
-      </CardHeader>
-      <CardContent></CardContent>
-      <CardFooter className="flex justify-between"></CardFooter>
-    </Card>
+    <div className=" group">
+      <div className="w-full h-45 overflow-hidden ">
+        <Image
+          width={300}
+          height={300}
+          src={image}
+          alt={alt}
+          className="w-full h-45 object-cover group-hover:scale-110 duration-300"
+        />
+      </div>
+
+      <h1 className="text-lg md:text-2xl font-semibold text-center   p-4">
+        {title}
+      </h1>
+      <p className="text-sm text-slate-500 border-t-2 border-b-2 border-slate-100 py-1 px-4 text-center capitalize">
+        {date}
+      </p>
+    </div>
   );
 };
 
